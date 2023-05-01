dbab0e36567c6f24a0c28e4d132fc2b5ac31b104
hudahamid <huda.00091581@gmail.com>
2023-05-01 15:09:49 +0300

    unique-key

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,14 +1,39 @@
-import React from 'react'
-
+import React ,{ useState } from 'react'; 
 import { Link } from 'react-router-dom'
+import { users } from './users-db'
 function Users() {
+  const usersList = useState(users);
   return (
-    <div>
+     <div>
       <h2>Users info</h2>
       <div>
-        tables
+      <table>
+      <thead>
+        <tr>
+          <th >Name</th>
+          <th >Role</th>
+          <th >Creation Date</th>
+          <th>Access</th>
+        </tr>
+      </thead>
+      <tbody>
+        {usersList.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.role}</td>
+            <td>{user.creationDate}</td>
+            <td>
+              <button >
+               enable
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
       </div>
       <div>
+        <br/>
         <button><Link to="/addUsers">Add new users</Link></button>
       </div>
     </div>
